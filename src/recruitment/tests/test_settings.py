d0aceb4e08270747d34986d39e01c31fedc4c857
Klaudia Ryciuk <rinrearoa@gmail.com>
2023-11-26 19:25:04 +0100

    chore: update pre-commit

--- src/recruitment/tests/test_settings.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-from django.contrib.auth.password_validation import validate_password
-from django.test import SimpleTestCase
-
-
-class TestSettings(SimpleTestCase):
-    def test_secret_key_strength(self):
-        validate_password(settings.SECRET_KEY)
