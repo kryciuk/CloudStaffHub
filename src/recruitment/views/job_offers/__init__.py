35c4bdf4c6aeb119103422a98440d52e93ec07b9
Klaudia Ryciuk <rinrearoa@gmail.com>
2024-01-02 21:59:34 +0100

    feat: add __all__ defintion to __init__.py file

--- src/recruitment/views/job_offers/__init__.py
+++ src/recruitment/views/job_offers/__init__.py
@@ -1,6 +1,14 @@
-from .job_offers_list import JobOffersListView
 from .job_offers_apply import JobOffersApplyView
-from .job_offers_create import JobOffersCreateView
+from .job_offers_create import JobOffersCreateView, load_cities
 from .job_offers_detail import JobOffersDetailView
+from .job_offers_list import JobOffersListView
 from .job_offers_update import JobOffersUpdateView
-from .job_offers_create import load_cities
+
+__all__ = [
+    "JobOffersListView",
+    "JobOffersApplyView",
+    "JobOffersCreateView",
+    "JobOffersDetailView",
+    "JobOffersUpdateView",
+    "load_cities",
+]
