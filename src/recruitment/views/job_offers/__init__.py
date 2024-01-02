c073fb0f25d1ce9bc9ce3f30fb9e92852eb680e4
Klaudia Ryciuk <rinrearoa@gmail.com>
2024-01-02 15:50:23 +0100

    feat: enable country and city selection for new job offers

---
+++ src/recruitment/views/job_offers/__init__.py
@@ -0,0 +1,6 @@
+from .job_offers_list import JobOffersListView
+from .job_offers_apply import JobOffersApplyView
+from .job_offers_create import JobOffersCreateView
+from .job_offers_detail import JobOffersDetailView
+from .job_offers_update import JobOffersUpdateView
+from .job_offers_create import load_cities
