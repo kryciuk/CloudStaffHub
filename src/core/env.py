6fc190d078796badbda18c479afcfd1d0644dff4
Klaudia Ryciuk <rinrearoa@gmail.com>
2024-01-16 21:53:17 +0100

    chore: update settings

--- src/core/env.py
+++ src/core/env.py
@@ -1,4 +1,4 @@
 import environ
 from django.core.management.utils import get_random_secret_key
 
-env = environ.Env(DEBUG=(bool, False), SECRET_KEY=(str, get_random_secret_key()))
+env = environ.Env(DEBUG=(bool, False), SECRET_KEY=(str, get_random_secret_key()), ENVIRONMENT=(str, "local"))
--- src/core/settings.py
+++ src/core/settings.py
@@ -105,17 +105,24 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": env("DB_NAME"),
-        "USER": env("DB_USER"),
-        "PASSWORD": env("DB_PASSWORD"),
-        "HOST": env("DB_HOST"),
-        "PORT": env("DB_PORT"),
+if env("ENVIROMENT") == "ci":
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": "ci_db",
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": env("DB_NAME"),
+            "USER": env("DB_USER"),
+            "PASSWORD": env("DB_PASSWORD"),
+            "HOST": env("DB_HOST"),
+            "PORT": env("DB_PORT"),
+        }
+    }
 
 
 # Password validation
