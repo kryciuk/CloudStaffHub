43c284df0f951206cfd707884c57590731ac0e36
Klaudia Ryciuk <rinrearoa@gmail.com>
2024-01-27 19:17:11 +0100

    chore: add prefixes to address Django anti-pattern

--- src/organizations/views/company_register.py
+++ src/organizations/views/company_register.py
@@ -12,13 +12,13 @@ from users.forms import CreateUserForm
 
 class RegisterCompanyView(View):
     def post(self, request):
-        form_company = CompanyForm(request.POST)
-        form = CreateUserForm(request.POST)
+        form_company = CompanyForm(request.POST, prefix="company")
+        form = CreateUserForm(request.POST, prefix="user")
         if not form_company.is_valid() or not form.is_valid():
             messages.warning(request, make_nice_error_keys(form_company.errors))
             messages.warning(request, make_nice_error_keys(form.errors))
             return redirect("register-company")
-        if form.is_valid() and form_company.is_valid():
+        else:
             domain_user = form.clean_email().rsplit("@")[1]
             domain_company = form_company.clean_email_domain()
             if domain_user != domain_company:
@@ -35,11 +35,9 @@ class RegisterCompanyView(View):
     def get(self, request):
         if request.user.is_authenticated:
             return redirect("dashboard-employee")
-        form_company = CompanyForm()
-        form = CreateUserForm()
         context = {
-            "form_company": form_company,
-            "form": form,
+            "form_company": CompanyForm(prefix="company"),
+            "form": CreateUserForm(prefix="user"),
             "title": "Register Company - Cloud Staff Hub",
         }
         return render(request, "organizations/register_company.html", context)
