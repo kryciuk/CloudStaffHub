cc9eb33336e0beaceb46e6c8fd6ebcfe3fcbe59c
Klaudia Ryciuk <rinrearoa@gmail.com>
2024-01-27 20:32:38 +0100

    test: write organizations company form tests

--- src/organizations/tests/test_forms.py
+++ src/organizations/tests/test_forms.py
@@ -10,3 +10,13 @@ class TestCompanyFormForm(TestCase):
     def test_if_company_is_created_if_correct_data(self):
         form = CompanyForm(data=self.form_data)
         self.assertTrue(form.is_valid())
+
+    def test_if_company_is_not_created_if_incorrect_data(self):
+        self.form_data["email_domain"] = "abc"
+        form = CompanyForm(data=self.form_data)
+        self.assertFalse(form.is_valid())
+
+    def test_if_company_is_not_created_if_missing_data(self):
+        self.form_data.pop("email_domain")
+        form = CompanyForm(data=self.form_data)
+        self.assertFalse(form.is_valid())
