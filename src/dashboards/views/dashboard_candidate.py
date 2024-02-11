ed9469acd27bb7a89da5764b592f20d1dff1e59b
Klaudia Ryciuk <rinrearoa@gmail.com>
2024-02-11 20:29:29 +0100

    feat: add random articles and job hunting tips using api in candidate's dashboards

--- src/dashboards/views/dashboard_candidate.py
+++ src/dashboards/views/dashboard_candidate.py
@@ -1,7 +1,10 @@
+import random
 from random import randrange
 
+import requests
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
+from django.utils import timezone
 from django.views.generic import TemplateView
 
 from organizations.models import Company
@@ -55,5 +58,30 @@ class CandidateDashboardView(LoginRequiredMixin, TemplateView):
         context["newest_entries"] = proposed_job_offers
         context["newest_entries_department"] = proposed_job_offers_by_department
         context["newest_entries_field"] = proposed_job_offers_by_field
+
+        # articles tips job hunting
+
+        results_tips = requests.get(
+            "https://newsapi.org/v2/everything?q=job+hunting+tips&apiKey=063c8ff3b9ab476297774505a481006d"
+        ).json()
+        articles_tips = results_tips["articles"]
+        random_article_tips = random.randrange(0, len(articles_tips))
+        article_tips = articles_tips[random_article_tips]
+        context["article_tips"] = article_tips
+
+        # articles interested in
+
+        yesterday = timezone.datetime.now() - timezone.timedelta(days=1)
+        oldest_article_date = yesterday.strftime("%Y-%m-%d")
+
+        industry = user_interested_in_field.industry
+        results_industry = requests.get(
+            f"https://newsapi.org/v2/everything?q={industry}&sortby=relevancy&language=en&from={oldest_article_date}&apiKey=063c8ff3b9ab476297774505a481006d"
+        ).json()
+        articles_industry = results_industry["articles"]
+        most_relevant = round(len(articles_industry) * 0.3)
+        random_article_industry = random.randrange(0, most_relevant)
+        article_industry = articles_industry[random_article_industry]
+        context["article_industry"] = article_industry
 
         return context
--- src/dashboards/views/dashboard_owner.py
+++ src/dashboards/views/dashboard_owner.py
@@ -1,6 +1,10 @@
+import random
+
+import requests
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
 from django.views.generic import TemplateView
+from icecream import ic
 
 
 class UserHasOwnerOrHigherGroup(LoginRequiredMixin, UserPassesTestMixin):
@@ -17,4 +21,17 @@ class OwnerDashboardView(UserHasOwnerOrHigherGroup, TemplateView):
         context = super().get_context_data(**kwargs)
         company = self.request.user.profile.company
         context["employees"] = User.objects.filter(profile__company=company).order_by("-id")[:10]
+
+        # news
+
+        articles = requests.get(
+            "https://newsapi.org/v2/top-headlines/sources?category=business&country=us&apiKey=063c8ff3b9ab476297774505a481006d"
+        ).json()
+        ic(len(articles["sources"]))
+        random_article = random.randrange(0, len(articles["sources"]))
+        ic(articles["sources"][random_article])
+        article = articles["sources"][random_article]
+
+        context["article"] = article
+
         return context
