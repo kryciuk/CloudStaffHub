b9d642a0fc87e4c177279f65bd60200e95cba4a9
Klaudia Ryciuk <rinrearoa@gmail.com>
2024-06-25 19:15:57 +0200

    chore: add aws files

--- src/core/settings.py
+++ src/core/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1 0.0.0.0:8000"]
+ALLOWED_HOSTS = ['*']
 
 X_FRAME_OPTIONS = "SAMEORIGIN"
 
