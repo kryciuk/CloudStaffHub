5f97cc6635ac78bbfbe21d93cd78b7fd4550fbcc
Klaudia Ryciuk <rinrearoa@gmail.com>
2024-06-28 15:53:24 +0200

    chore: add aws files

--- src/core/settings.py
+++ src/core/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 env.read_env(os.path.join(BASE_DIR.parent, ".env"))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 
