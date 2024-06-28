b4fb39113f8bd4f5550a26a27e8eab0581dc25f4
Klaudia Ryciuk <rinrearoa@gmail.com>
2024-06-28 15:39:31 +0200

    chore: add aws files

--- src/core/settings.py
+++ src/core/settings.py
@@ -256,59 +256,6 @@ BOOTSTRAP_DATEPICKER_PLUS = {
     },
 }
 
-# LOGGING = {
-#     "version": 1,
-#     "disable_existing_loggers": False,
-#     "formatters": {
-#         "verbose": {
-#             "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
-#             "style": "{",
-#         },
-#         "simple": {
-#             "format": "{levelname} {message}",
-#             "style": "{",
-#         },
-#     },
-#     "filters": {
-#         "special": {
-#             "()": "project.logging.SpecialFilter",
-#             "foo": "bar",
-#         },
-#         "require_debug_true": {
-#             "()": "django.utils.log.RequireDebugTrue",
-#         },
-#     },
-#     "handlers": {
-#         "console": {
-#             "level": "INFO",
-#             "filters": ["require_debug_true"],
-#             "class": "logging.StreamHandler",
-#             "formatter": "simple",
-#         },
-#         "mail_admins": {
-#             "level": "ERROR",
-#             "class": "django.utils.log.AdminEmailHandler",
-#             "filters": ["special"],
-#         },
-#     },
-#     "loggers": {
-#         "django": {
-#             "handlers": ["console"],
-#             "propagate": True,
-#         },
-#         "django.request": {
-#             "handlers": ["mail_admins"],
-#             "level": "ERROR",
-#             "propagate": False,
-#         },
-#         "CSH": {
-#             "handlers": ["console", "mail_admins"],
-#             "level": "INFO",
-#             "filters": ["special"],
-#         },
-#     },
-# }
-
 AUTHENTICATION_BACKENDS = (
     "django.contrib.auth.backends.ModelBackend",
     "guardian.backends.ObjectPermissionBackend",
