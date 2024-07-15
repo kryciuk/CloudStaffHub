ac40e8bee8d75aaa2773d441b0af95b094eb6fe5
Klaudia Ryciuk <rinrearoa@gmail.com>
2024-07-15 22:07:42 +0200

    fix: fix patch to media

--- src/core/settings/common.py
+++ src/core/settings/common.py
@@ -142,7 +142,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 LOGIN_URL = "login"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "../../media")
+MEDIA_ROOT = os.path.join(BASE_DIR.parent.parent, "src", "media")
 
 MEDIA_URL = "/media/"
 
